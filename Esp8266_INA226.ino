4155e023be4925d2df81b90149ff3c88c773adea
Sergey Romanyuk <sergey.romanyuk@dataart.com>
2018-10-16 00:16:16 +0300

    Sensor reading/publishing added

--- Esp8266_INA226.ino
+++ Esp8266_INA226.ino
@@ -4,6 +4,11 @@
 #include <ESP8266httpUpdate.h>
 #include <Credentials\Credentials.h>
 
+//---------------- INA226------------------
+#include <Wire.h>
+#include <INA226.h>
+//-----------------------------------------
+
 
 const char* mqtt_server = SERVER_IP;
 WiFiClient espClient;
@@ -15,9 +20,23 @@ PubSubClient client(espClient);
 long lastTempMsg = 0;
 char msg[50];
 
-int sensorRequestPeriod = 60000; // 60 seconds
+int sensorRequestPeriod = 20000; // 20 seconds
 
-const int RELAY_PIN = 16;
+const int RELAY_PIN = 0; //GPIO 0 or D3
+//const int RELAY_PIN = 16;
+//const int RELAY_PIN = 16;
+//const int RELAY_PIN = 16;
+
+
+
+//---------------- INA226------------------
+INA226 ina;
+float busVoltage = 0.0;
+float busPower = 0.0;
+float shuntVoltage = 0.0;
+float shuntCurrent = 0.0;
+
+//-----------------------------------------
 
 /* Over The Air automatic firmware update from a web server.  ESP8266 will contact the
 *  server on every boot and check for a firmware update.  If available, the update will
@@ -32,12 +51,15 @@ const int RELAY_PIN = 16;
 
 void setup()
 {
+	pinMode(RELAY_PIN, OUTPUT);
+	digitalWrite(RELAY_PIN, HIGH);
 	Serial.begin(115200);
 	WiFi.mode(WIFI_STA);
 	client.setServer(mqtt_server, 1883);
 	client.setCallback(callback);
 	setup_wifi();
-	HTTP_OTA();
+	//HTTP_OTA();
+	initializeINA226();
 }
 
 void setup_wifi() {
@@ -47,9 +69,8 @@ void setup_wifi() {
 	Serial.print(F("Connecting to "));
 	Serial.println(SSID);
 	WiFi.begin(SSID, PASSWORD);
-	delay(1000);
-	Serial.print(F("IP address: "));
-	Serial.println(WiFi.localIP());
+	delay(3000);
+
 	if (WiFi.waitForConnectResult() != WL_CONNECTED) {
 		Serial.println(F("Connection Failed!"));
 		return;
@@ -91,7 +112,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
 		Serial.print((char)payload[i]);
 	}
 	Serial.println("-----");
-	if (strcmp(topic, "Sender_1/topic_1") == 0) {
+	if (strcmp(topic, "Battery/relay_1") == 0) {
 		//Switch on the RELAY if an 1 was received as first character
 		if ((char)payload[0] == '1') {
 			digitalWrite(RELAY_PIN, LOW);   // Turn the RELAY on
@@ -100,13 +121,13 @@ void callback(char* topic, byte* payload, unsigned int length) {
 			digitalWrite(RELAY_PIN, HIGH);  // Turn the RELAY off
 		}
 	}
-	if (strcmp(topic, "Sender_2/topic_2") == 0) {
+	if (strcmp(topic, "Battery/sensorRequestPeriod") == 0) {
 		String myString = String((char*)payload);
 		Serial.println(myString);
 		sensorRequestPeriod = myString.toInt();
 		Serial.print("Sensor request period set to :");
 		Serial.print(sensorRequestPeriod);
-		Serial.println("ms");
+		Serial.println(" ms");
 	}
 }
 
@@ -114,13 +135,13 @@ void callback(char* topic, byte* payload, unsigned int length) {
 void connectToBroker() {
 	Serial.print("Attempting MQTT connection...");
 	// Attempt to connect
-	if (client.connect("Client_Name")) {
+	if (client.connect("Battery")) {
 		Serial.println("connected");
 		// Once connected, publish an announcement...
-		client.publish("Client_Name/status", "Wemos D1 mini connected");
+		client.publish("Battery/status", "Wemos D1 mini on battery connected");
 		// ... and resubscribe
-		client.subscribe("Sender_1/topic_1");
-		client.subscribe("Sender_2/topic_2");
+		client.subscribe("Battery/relay_1");
+		client.subscribe("Battery/sensorRequestPeriod");
 	}
 	else {
 		Serial.print("failed, rc=");
@@ -164,20 +185,150 @@ void loop()
 	}
 	client.loop();
 	sendMessageToMqtt();
+	//Serial.print("Go to deep sleep");
+	//ESP.deepSleep(60e6); // 20e6 is 20 microseconds
 }
 
 void sendMessageToMqtt() {
 	long now = millis();
 	if (now - lastTempMsg > sensorRequestPeriod) {
 		lastTempMsg = now;
-		float sensor_value = 30;
-		// Request sensot here
-		//float temp = sensors.getTempCByIndex(0);
-		Serial.print("Publish message temp: ");
-		Serial.println(sensor_value);
-		if (-20 < sensor_value && sensor_value < 50) // do not send failed read data
+		getSensorData();
+
+		Serial.print("Publish message busVoltage: ");
+		Serial.println(busVoltage, 5);
+		if (0 < busVoltage && busVoltage < 20) // do not send failed read data
 		{
-			client.publish("Battery/voltage", String(sensor_value).c_str());
+			client.publish("Battery/busVoltage", String(busVoltage, 5).c_str());
 		}
+
+		Serial.print("Publish message busPower: ");
+		Serial.println(busPower, 5);
+		if (0 < busPower && busPower < 100) // do not send failed read data
+		{
+			client.publish("Battery/busPower", String(busPower, 5).c_str());
+		}
+
+		Serial.print("Publish message shuntCurrent: ");
+		Serial.println(shuntCurrent, 5);
+		client.publish("Battery/shuntCurrent", String(shuntCurrent, 5).c_str());
+
+		Serial.print("Publish message shuntVoltage: ");
+		Serial.println(shuntVoltage, 5);
+		client.publish("Battery/shuntVoltage", String(shuntVoltage, 5).c_str());
+
 	}
 }
+void initializeINA226() {
+	//Serial.println("Initialize INA226");
+	//Serial.println("-----------------------------------------------");
+	// Default INA226 address is 0x40
+	ina.begin();
+	// Configure INA226
+	ina.configure(INA226_AVERAGES_128, INA226_BUS_CONV_TIME_4156US, INA226_SHUNT_CONV_TIME_4156US, INA226_MODE_SHUNT_BUS_CONT);
+	// Calibrate INA226. Rshunt = 0.1 ohm, Max excepted current = 2A
+	ina.calibrate(0.1, 2);
+	// Display configuration
+	//checkINA226Config();
+	//Serial.println("-----------------------------------------------");
+}
+
+void checkINA226Config()
+{
+	Serial.print("Mode:                  ");
+	switch (ina.getMode())
+	{
+	case INA226_MODE_POWER_DOWN:      Serial.println("Power-Down"); break;
+	case INA226_MODE_SHUNT_TRIG:      Serial.println("Shunt Voltage, Triggered"); break;
+	case INA226_MODE_BUS_TRIG:        Serial.println("Bus Voltage, Triggered"); break;
+	case INA226_MODE_SHUNT_BUS_TRIG:  Serial.println("Shunt and Bus, Triggered"); break;
+	case INA226_MODE_ADC_OFF:         Serial.println("ADC Off"); break;
+	case INA226_MODE_SHUNT_CONT:      Serial.println("Shunt Voltage, Continuous"); break;
+	case INA226_MODE_BUS_CONT:        Serial.println("Bus Voltage, Continuous"); break;
+	case INA226_MODE_SHUNT_BUS_CONT:  Serial.println("Shunt and Bus, Continuous"); break;
+	default: Serial.println("unknown");
+	}
+
+	Serial.print("Samples average:       ");
+	switch (ina.getAverages())
+	{
+	case INA226_AVERAGES_1:           Serial.println("1 sample"); break;
+	case INA226_AVERAGES_4:           Serial.println("4 samples"); break;
+	case INA226_AVERAGES_16:          Serial.println("16 samples"); break;
+	case INA226_AVERAGES_64:          Serial.println("64 samples"); break;
+	case INA226_AVERAGES_128:         Serial.println("128 samples"); break;
+	case INA226_AVERAGES_256:         Serial.println("256 samples"); break;
+	case INA226_AVERAGES_512:         Serial.println("512 samples"); break;
+	case INA226_AVERAGES_1024:        Serial.println("1024 samples"); break;
+	default: Serial.println("unknown");
+	}
+
+	Serial.print("Bus conversion time:   ");
+	switch (ina.getBusConversionTime())
+	{
+	case INA226_BUS_CONV_TIME_140US:  Serial.println("140uS"); break;
+	case INA226_BUS_CONV_TIME_204US:  Serial.println("204uS"); break;
+	case INA226_BUS_CONV_TIME_332US:  Serial.println("332uS"); break;
+	case INA226_BUS_CONV_TIME_588US:  Serial.println("558uS"); break;
+	case INA226_BUS_CONV_TIME_1100US: Serial.println("1.100ms"); break;
+	case INA226_BUS_CONV_TIME_2116US: Serial.println("2.116ms"); break;
+	case INA226_BUS_CONV_TIME_4156US: Serial.println("4.156ms"); break;
+	case INA226_BUS_CONV_TIME_8244US: Serial.println("8.244ms"); break;
+	default: Serial.println("unknown");
+	}
+
+	Serial.print("Shunt conversion time: ");
+	switch (ina.getShuntConversionTime())
+	{
+	case INA226_SHUNT_CONV_TIME_140US:  Serial.println("140uS"); break;
+	case INA226_SHUNT_CONV_TIME_204US:  Serial.println("204uS"); break;
+	case INA226_SHUNT_CONV_TIME_332US:  Serial.println("332uS"); break;
+	case INA226_SHUNT_CONV_TIME_588US:  Serial.println("558uS"); break;
+	case INA226_SHUNT_CONV_TIME_1100US: Serial.println("1.100ms"); break;
+	case INA226_SHUNT_CONV_TIME_2116US: Serial.println("2.116ms"); break;
+	case INA226_SHUNT_CONV_TIME_4156US: Serial.println("4.156ms"); break;
+	case INA226_SHUNT_CONV_TIME_8244US: Serial.println("8.244ms"); break;
+	default: Serial.println("unknown");
+	}
+
+	Serial.print("Max possible current:  ");
+	Serial.print(ina.getMaxPossibleCurrent());
+	Serial.println(" A");
+
+	Serial.print("Max current:           ");
+	Serial.print(ina.getMaxCurrent());
+	Serial.println(" A");
+
+	Serial.print("Max shunt voltage:     ");
+	Serial.print(ina.getMaxShuntVoltage());
+	Serial.println(" V");
+
+	Serial.print("Max power:             ");
+	Serial.print(ina.getMaxPower());
+	Serial.println(" W");
+}
+
+void getSensorData() {
+	busVoltage = ina.readBusVoltage();
+	Serial.print("Bus voltage:   ");
+	Serial.print(busVoltage, 5);
+	Serial.println(" V");
+
+	busPower = ina.readBusPower();
+	Serial.print("Bus power:     ");
+	Serial.print(busPower, 5);
+	Serial.println(" W");
+
+	shuntVoltage = ina.readShuntVoltage();
+	Serial.print("Shunt voltage: ");
+	Serial.print(shuntVoltage, 5);
+	Serial.println(" V");
+
+	shuntCurrent = ina.readShuntCurrent();
+	Serial.print("Shunt current: ");
+	Serial.print(shuntCurrent, 5);
+	Serial.println(" A");
+
+	Serial.println("");
+}
+
