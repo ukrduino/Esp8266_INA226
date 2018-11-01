adca8d0049c5959f70cf09ae085ea672c7378964
Sergey Romanyuk <sergey.romanyuk@dataart.com>
2018-11-01 09:04:10 +0200

    Clean up

--- Esp8266_INA226.ino
+++ Esp8266_INA226.ino
@@ -8,8 +8,7 @@
 //---------------- INA226------------------
 #include <Wire.h>
 #include <INA226.h>
-//---------------Deep sleep settings to SPIFFS-----------------------
-#include "FS.h"
+
 
 
 const char* mqtt_server = SERVER_IP;
@@ -53,10 +52,8 @@ int sleepPeriod = 60; // Seconds
 void setup()
 {
 	Serial.begin(115200);
-	// always use this to "mount" the filesystem
-	bool result = SPIFFS.begin();
-	Serial.println("SPIFFS started: " + result);
 	pinMode(RELAY_PIN, OUTPUT);
+	pinMode(BUILTIN_LED, OUTPUT);
 	digitalWrite(RELAY_PIN, HIGH);
 	WiFi.mode(WIFI_STA);
 	client.setServer(mqtt_server, 1883);
@@ -205,6 +202,9 @@ void sendMessageToMqttInLoop() {
 
 void sendMessageToMqttOnce() {
 	if (useDeepSleep) {
+		//digitalWrite(BUILTIN_LED, LOW);
+		//delay(5000);
+		//digitalWrite(BUILTIN_LED, HIGH);
 		getSensorData();
 		sendMessageToMqtt();
 		sleep(sleepPeriod);
@@ -237,93 +237,15 @@ void sleep(int sleepTimeInSeconds) {
 }
 
 void initializeINA226() {
-	//Serial.println("Initialize INA226");
-	//Serial.println("-----------------------------------------------");
 	// Default INA226 address is 0x40
 	ina.begin();
 	// Configure INA226
-	ina.configure(INA226_AVERAGES_128, INA226_BUS_CONV_TIME_4156US, INA226_SHUNT_CONV_TIME_4156US, INA226_MODE_SHUNT_BUS_CONT);
+	ina.configure(INA226_AVERAGES_128, INA226_BUS_CONV_TIME_8244US, INA226_SHUNT_CONV_TIME_8244US, INA226_MODE_SHUNT_BUS_CONT);
 	// Calibrate INA226. Rshunt = 0.1 ohm, Max excepted current = 2A
 	ina.calibrate(0.1, 2);
-	// Display configuration
-	//checkINA226Config();
-	//Serial.println("-----------------------------------------------");
 }
 
-void checkINA226Config()
-{
-	Serial.print("Mode:                  ");
-	switch (ina.getMode())
-	{
-	case INA226_MODE_POWER_DOWN:      Serial.println("Power-Down"); break;
-	case INA226_MODE_SHUNT_TRIG:      Serial.println("Shunt Voltage, Triggered"); break;
-	case INA226_MODE_BUS_TRIG:        Serial.println("Bus Voltage, Triggered"); break;
-	case INA226_MODE_SHUNT_BUS_TRIG:  Serial.println("Shunt and Bus, Triggered"); break;
-	case INA226_MODE_ADC_OFF:         Serial.println("ADC Off"); break;
-	case INA226_MODE_SHUNT_CONT:      Serial.println("Shunt Voltage, Continuous"); break;
-	case INA226_MODE_BUS_CONT:        Serial.println("Bus Voltage, Continuous"); break;
-	case INA226_MODE_SHUNT_BUS_CONT:  Serial.println("Shunt and Bus, Continuous"); break;
-	default: Serial.println("unknown");
-	}
 
-	Serial.print("Samples average:       ");
-	switch (ina.getAverages())
-	{
-	case INA226_AVERAGES_1:           Serial.println("1 sample"); break;
-	case INA226_AVERAGES_4:           Serial.println("4 samples"); break;
-	case INA226_AVERAGES_16:          Serial.println("16 samples"); break;
-	case INA226_AVERAGES_64:          Serial.println("64 samples"); break;
-	case INA226_AVERAGES_128:         Serial.println("128 samples"); break;
-	case INA226_AVERAGES_256:         Serial.println("256 samples"); break;
-	case INA226_AVERAGES_512:         Serial.println("512 samples"); break;
-	case INA226_AVERAGES_1024:        Serial.println("1024 samples"); break;
-	default: Serial.println("unknown");
-	}
-
-	Serial.print("Bus conversion time:   ");
-	switch (ina.getBusConversionTime())
-	{
-	case INA226_BUS_CONV_TIME_140US:  Serial.println("140uS"); break;
-	case INA226_BUS_CONV_TIME_204US:  Serial.println("204uS"); break;
-	case INA226_BUS_CONV_TIME_332US:  Serial.println("332uS"); break;
-	case INA226_BUS_CONV_TIME_588US:  Serial.println("558uS"); break;
-	case INA226_BUS_CONV_TIME_1100US: Serial.println("1.100ms"); break;
-	case INA226_BUS_CONV_TIME_2116US: Serial.println("2.116ms"); break;
-	case INA226_BUS_CONV_TIME_4156US: Serial.println("4.156ms"); break;
-	case INA226_BUS_CONV_TIME_8244US: Serial.println("8.244ms"); break;
-	default: Serial.println("unknown");
-	}
-
-	Serial.print("Shunt conversion time: ");
-	switch (ina.getShuntConversionTime())
-	{
-	case INA226_SHUNT_CONV_TIME_140US:  Serial.println("140uS"); break;
-	case INA226_SHUNT_CONV_TIME_204US:  Serial.println("204uS"); break;
-	case INA226_SHUNT_CONV_TIME_332US:  Serial.println("332uS"); break;
-	case INA226_SHUNT_CONV_TIME_588US:  Serial.println("558uS"); break;
-	case INA226_SHUNT_CONV_TIME_1100US: Serial.println("1.100ms"); break;
-	case INA226_SHUNT_CONV_TIME_2116US: Serial.println("2.116ms"); break;
-	case INA226_SHUNT_CONV_TIME_4156US: Serial.println("4.156ms"); break;
-	case INA226_SHUNT_CONV_TIME_8244US: Serial.println("8.244ms"); break;
-	default: Serial.println("unknown");
-	}
-
-	Serial.print("Max possible current:  ");
-	Serial.print(ina.getMaxPossibleCurrent());
-	Serial.println(" A");
-
-	Serial.print("Max current:           ");
-	Serial.print(ina.getMaxCurrent());
-	Serial.println(" A");
-
-	Serial.print("Max shunt voltage:     ");
-	Serial.print(ina.getMaxShuntVoltage());
-	Serial.println(" V");
-
-	Serial.print("Max power:             ");
-	Serial.print(ina.getMaxPower());
-	Serial.println(" W");
-}
 
 void getSensorData() {
 	busVoltage = ina.readBusVoltage();
@@ -347,36 +269,4 @@ void getSensorData() {
 	Serial.println(" A");
 
 	Serial.println("");
-}
-
-int getSleepPeriodFromFile() {
-	int slPeriod = -1;
-	File sp = SPIFFS.open("/sp.txt", "r");
-	if (sp) {
-		String str = sp.readStringUntil('n');
-		slPeriod = str.toInt();
-		Serial.println("sp.txt read result: " + slPeriod);
-		sp.close();
-		return slPeriod;
-	}
-	Serial.println("sp.txt read failed!");
-	return slPeriod;
-}
-
-void saveSleepPeriodToFile(int slPer) {
-	if (getSleepPeriodFromFile() != slPer)
-	{
-		if (SPIFFS.remove("/sp.txt")) {
-			Serial.println("Old file succesfully deleted");
-		}
-		else {
-			Serial.println("Couldn't delete file");
-		}
-		File sp = SPIFFS.open("/sp.txt", "w");
-		if (!sp) {
-			Serial.println("file creation failed");
-		}
-		sp.println(slPer);
-		sp.close();
-	}	
 }
